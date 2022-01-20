20ee7bd832f5d39a1f6d282dafd05999df065693
tylereriksen <saeji.hong@gmail.com>
2022-01-19 20:18:32 -0800

    BreakPointFix

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,16 +10,23 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            System.out.println(currentIndex);
+        while(markdown.indexOf("[", currentIndex) != -1) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if(markdown.charAt(nextOpenBracket - 1) != '!'){
+                int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+                int openParen = markdown.indexOf("(", nextCloseBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+
+            else{
+                int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+                int openParen = markdown.indexOf("(", nextCloseBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                currentIndex = closeParen + 1;
+            }
         }
-        System.out.println(currentIndex);
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
