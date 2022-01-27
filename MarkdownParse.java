9f2e0ae5a07f6a534c16bbbf2563ace4774537ed
tylereriksen <saeji.hong@gmail.com>
2022-01-26 20:48:52 -0800

    FinalFinal

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -24,6 +24,7 @@ public class MarkdownParse {
                 int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
                 int openParen = markdown.indexOf("(", nextCloseBracket);
                 int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen))
                 currentIndex = closeParen + 1;
             }
         }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,27 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void markdownParseTester() throws IOException{
+        ArrayList<String> toReturn = new ArrayList<>(2);
+        toReturn.add("https://something.com");
+        toReturn.add("some-page.html");
+
+        Path fileName = Path.of("test-file.md");
+        String contents = Files.readString(fileName);   
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(toReturn.get(0), links.get(0));
+        assertEquals(toReturn.get(1), links.get(1));
+
     }
 }
