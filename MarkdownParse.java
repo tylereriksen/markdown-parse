5d356d191699aa9ca421f7d71427a31fa1ce0b2f
tylereriksen <saeji.hong@gmail.com>
2022-01-26 22:13:36 -0800

    LastEdit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,21 +10,26 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        while(markdown.indexOf("[", currentIndex) != -1 && currentIndex < markdown.length()) {
+        while(markdown.indexOf("[", currentIndex) != -1) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            if(markdown.charAt(nextOpenBracket - 1) != '!'){
+            if(nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!'){
                 int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
                 int openParen = markdown.indexOf("(", nextCloseBracket);
                 int closeParen = markdown.indexOf(")", openParen);
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
+                if(!markdown.substring(openParen + 1, closeParen).contains(" ")){
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                    currentIndex = closeParen + 1;
+                }
+                else{
+                    currentIndex = closeParen + 1;
+                }
             }
+            
 
             else{
                 int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
                 int openParen = markdown.indexOf("(", nextCloseBracket);
                 int closeParen = markdown.indexOf(")", openParen);
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
                 currentIndex = closeParen + 1;
             }
         }
